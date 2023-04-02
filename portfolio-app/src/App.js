0201d4176214ca74b139f380c68c2ea888a5e26d
R-Rangel <r.rangel.0222@gmail.com>
2023-04-02 04:59:50 -0500

    adding about component and trying to get it to work

--- portfolio-app/src/App.js
+++ portfolio-app/src/App.js
@@ -1,10 +1,13 @@
 import Navbar from "./components/navbar";
 import Main from "./components/Main";
+import About from "./components/About";
+
 function App() {
   return (
     <div>      
   <Navbar />
   <Main />
+  <About />
         </div>
   );
 }

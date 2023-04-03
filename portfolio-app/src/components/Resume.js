0c162bd09b91a17dfa019dcab3c516410ed87280
R-Rangel <r.rangel.0222@gmail.com>
2023-04-02 22:57:15 -0500

    i have added in the contact, about, portfolio, and resume js

---
+++ portfolio-app/src/components/Resume.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Resume = () => {
+  return (
+    <div>
+      <img src="/assets/resume.png" alt="Resume"/>
+    </div>
+  )
+}
+
+export default Resume;
--- portfolio-app/src/components/navbar.js
+++ portfolio-app/src/components/navbar.js
@@ -1,16 +1,15 @@
 import React from 'react'
 
- const Navbar = () => {
+const Navbar = () => {
   return (
     <div className='fixed w-full h-[80px] flex justify-between items-center px-4 bg-[#0D1317] text-[#ffe4f2]'>
         
         <div className=''>
             <ul className='flex'>
-                
-                <li>About Me</li>
-                <li>Portfolio</li>
-                <li>Contact</li>
-                <li>Resume</li>
+                <li><a href="/">About Me</a></li>
+                <li><a href="/">Portfolio</a></li>
+                <li><a href="/">Contact</a></li>
+                <li><a href="/">Resume</a></li>
             </ul>
         </div>
        
